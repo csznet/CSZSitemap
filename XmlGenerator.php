49e68ad28e2fd030d6b18f7744e78788dd29dbe2
yimi <yimi8864@gmail.com>
2023-07-02 01:26:14 +0800

    change xml

--- XmlGenerator.php
+++ XmlGenerator.php
@@ -1,5 +1,5 @@
 <?php
-class CSZSitema_XmlGenerator extends Typecho_Widget implements Widget_Interface_Do
+class CSZSitemap_XmlGenerator extends Typecho_Widget implements Widget_Interface_Do
 {
 	public function action()
 	{
